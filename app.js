4e953ed4d362c3011b22bb4490f2828a031e7499
Yassine Bess <yazo-yazo@hotmail.com>
2022-05-30 14:43:33 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+async function getData(url) {
+    let res = await fetch(url);
+    return res.json();
+}
+let row = document.getElementById('data')
+getData("https://api.watchmode.com/v1/sources/?apiKey=RXnibDhDTacGWiE349ruwDd9loWC6FHMsmfxeyEb")
+    .then((data) => {
+
+        console.log(data);
+
+    })
+for (let i = 0; i < data.results.length; i++) {
+    let col = document.createElement('div');
+    col.classList.add('col-md-3', 'my-3', 'text-center')
+    let card = document.createElement('div');
+    card.classList.add('card', 'shadow', 'rounded')
+}
+// function a() {
+//     let cont = 0
+//     return function() {
+//         return cont++
+
+//     }
+
+// }
+// let res = a()
+// console.log(res());
